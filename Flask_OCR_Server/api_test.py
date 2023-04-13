b9969205e64d9567474067ce7b4ea95f536660bb
Alec Vince <aleclightning14@gmail.com>
2023-04-14 00:01:28 +0800

    Added Database access & Fuzzy Search
    - Using pymsql make numpy array for records
    - Using text distance made a new function for fuzzy search with Jira Winkler and Levenshtein Distance
    - Added API for updating records

--- Flask_OCR_Server/api_test.py
+++ Flask_OCR_Server/api_test.py
@@ -45,7 +45,7 @@ import base64
 # import cv2, numpy as np, pytesseract
 # from  pytesseract import Output
 
-with open("api_test.png", "rb") as f:
+with open("api_test5.png", "rb") as f:
     im_b64 = base64.b64encode(f.read())
 
 sent_image = im_b64.decode('utf-8')  
@@ -63,13 +63,13 @@ print(type(x.text))
 
 ############################################################################
 
-# ! Testing for Returning base64 image with bounding boxes and text
+# # # ! Testing for Returning base64 image with bounding boxes and text
 
 # import easyocr
 # from PIL import Image
 # import cv2 as cv, matplotlib as plt, numpy as np
 
-# im_file = "api_test.png"
+# im_file = "api_test6.png"
 
 # import base64
 
@@ -96,14 +96,28 @@ print(type(x.text))
 
 # image2 = img.copy()
 
+# number_test = 0
+
 # for bounding_boxes in result:
+
+#     print("\n",bounding_boxes)
+
 #     points = bounding_boxes[0]
-#     rect = cv.boundingRect(np.array(points))
+
+#     print(points)
+#     print(type(points[0][0]))
+
+#     numpy_array_points = np.array(points)
+
+#     rect = cv.boundingRect(numpy_array_points.astype(np.float32))
 #     x, y, w, h = rect
+
 #     cv.rectangle(image2, (x, y), (x + w, y + h), (0, 0, 255), 1)
 #     cv.putText(image2, bounding_boxes[1], (x, y-5), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
 
+#     number_test+=1
 
+    
 # retval, buffer = cv.imencode('.jpg', image2)
 # jpg_as_text = base64.b64encode(buffer)
 # print(jpg_as_text)
@@ -182,3 +196,228 @@ print(type(x.text))
 # print(a)
 # print(type(a))
 
+###################################################################################
+
+# #! request post json test database api
+
+# import requests
+
+# import base64
+
+# # import cv2, numpy as np, pytesseract
+# # from  pytesseract import Output
+ 
+
+# #url = 'http://127.0.0.1:5000/api/pytesseract/output/dictionary'
+# url = 'http://127.0.0.1:5000//api/medicinerecords/read'
+
+
+# x = requests.post(url)
+
+# #print the response text (the content of the requested file):
+
+# print(x.text)
+# print(type(x.text))
+
+########################################################################################
+
+# import easyocr
+# from PIL import Image
+# import cv2 as cv, matplotlib as plt, numpy as np
+
+# im_file = "api_test.png"
+
+# import base64
+
+# from io import BytesIO
+
+# with open(im_file, "rb") as f:
+#     sent_image = base64.b64encode(f.read())
+
+# sent_image = sent_image.decode('utf-8')  
+
+# str_decoded_bytes = bytes(sent_image, 'utf-8')
+
+# im_bytes = base64.b64decode(str_decoded_bytes)   # im_bytes is a binary image
+
+# reader = easyocr.Reader(['en'], gpu=True)
+# result = reader.readtext(im_bytes,detail=1)
+
+# print(result)
+
+# import numpy as np, textdistance as td
+
+# medicine = [['LORATADINE','10'],['LEVOCETIRIZINE','5'],['PANADOL','650']]
+
+# medicine_list = np.array(medicine)
+
+# ocr_results_actual = ["DEOJAJAS","AMPICILLIN","KODONAL","PADOL", "LORADOL", "BINEDOL"]
+
+# for item in ocr_results_actual:
+#     a = item.upper()
+
+#     item = a
+
+
+# test_strings = [
+#     'Panadol',         # exact match
+#     'Banadyl',         # high similarity
+#     'Pandalol',        # high similarity
+#     'Kodonal',         # medium similarity
+#     'Panaflam',        # low similarity
+#     'Ibuprofen',       # very low similarity
+#     'Pandalot',        # worst case scenario - only one letter in common
+#     'Ampicillin',      # worst case scenario - no letters in common
+#     'Pandynol',        # best case scenario - same length and only one transposition
+#     'Painadol'         # best case scenario - same length and no transpositions
+# ]
+
+# fuzzy_search_results = {}
+
+# number = 0
+
+# for word in ocr_results_actual:
+#     print(number)
+#     number+=1
+
+#     #scores assignment
+#     jw_best_score = 0.0
+#     ld_best_score = 0.0
+
+#     #best input for each algorithm
+#     jw_best_input = ""
+#     ld_best_input = ""
+
+#     for record in medicine_list:
+
+#         jw_score = td.jaro_winkler(word,record[0])
+#         ld_score = td.levenshtein.normalized_similarity(word, record[0])
+
+#         print(word,record[0], jw_score, ld_score)
+
+#         if jw_score > jw_best_score:
+
+#             jw_best_score = jw_score
+#             jw_best_input = word
+
+#             #output
+#             fuzzy_search_results["jw_best_match"] = record[0]
+
+#         if ld_score > ld_best_score:
+
+#             ld_best_score = ld_score
+#             ld_best_input = word
+
+#             #output
+#             fuzzy_search_results["ld_best_match"] = record[0]
+            
+# print(fuzzy_search_results)
+
+# print("\n",jw_best_score,fuzzy_search_results["jw_best_match"], jw_best_input)
+
+# print("\n",ld_best_score,fuzzy_search_results["ld_best_match"], ld_best_input)
+
+
+######################################################################
+
+# #list of symbols for preprocessing
+# list_of_symbols = ["™","®","©","&trade;","&reg;","&copy;","&#8482;","&#174;","&#169;","\n"]
+
+# import re, string
+
+# def textpreprocessing(textprocess):
+#     """
+#     Function to clean and preprocess list of text
+#     :param list of words:
+#     :return: List of cleaned words
+#     """
+#     #uppercase all the letters
+#     text_to_process = textprocess.upper()
+
+#     #remove the punctuations
+#     text_to_process = "".join([char for char in text_to_process if char not in string.punctuation])
+
+#     #remove other symbols
+#     for symbol in list_of_symbols:
+#         text_to_process = text_to_process.replace(symbol," ")
+
+#     #remove unicode
+#     text_to_process = text_to_process.encode("ascii", "ignore")
+#     text_to_process = text_to_process.decode()
+
+#     #regex remove not needed words
+#     list_of_matches = re.search("(\d+)(MG)|(\d+) (MG)|TABLET",text_to_process)
+
+#     while list_of_matches:
+#         text_to_process = text_to_process.replace(list_of_matches.group(),"")
+#         list_of_matches = re.search("(\d+)(MG)|(\d+) (MG)|TABLET",text_to_process)
+
+#     return text_to_process
+
+# list_of_words = list(textpreprocessing(" aksdfasdjhakjdhas ajsdajdhasjdas , sdhjashdsja ").split())
+
+# print(list_of_words)
+
+
+#################################################################################################
+
+# import textdistance as td
+
+# drug_records = [['LORATADINE','10'],['LEVOCETIRIZINE','5'],['PANADOL','650']]
+
+
+# def fuzzy_search(list_of_words, drug_records):
+#     """ 
+#     Function to fuzzy search algorithm of jaro winkler and levenshtein distance
+#     :param list of words list of records:
+#     :return: list of best score text for each algorithm
+#     """
+#     number = 0
+
+#     jw_best_match = ""
+#     ld_best_match = ""
+
+#     for word in list_of_words:
+#         print(number)
+#         number+=1
+
+#         #scores assignment
+#         jw_best_score = 0.0
+#         ld_best_score = 0.0
+
+#         #best input for each algorithm
+#         jw_best_input = ""
+#         ld_best_input = ""
+
+#         for record in drug_records:
+
+#             jw_score = td.jaro_winkler(word,record[0])
+#             ld_score = td.levenshtein.normalized_similarity(word, record[0])
+
+#             print(word,record[0], jw_score, ld_score)
+
+#             if jw_score > jw_best_score:
+
+#                 jw_best_score = jw_score
+#                 jw_best_input = word
+
+#                 #output
+#                 jw_best_match = record[0]
+
+#             if ld_score > ld_best_score:
+
+#                 ld_best_score = ld_score
+#                 ld_best_input = word
+
+#                 #output
+#                 ld_best_match = record[0]
+
+#         print("\n",jw_best_score,jw_best_match, jw_best_input)
+
+#         print("\n",ld_best_score,ld_best_match, ld_best_input)
+
+#         list_to_return = [jw_best_match,ld_best_match]
+
+#         return list_to_return
+    
+# print(fuzzy_search(['', 'PANADOL', '', 'MEDICINE', '2', '', 'MEDICINERE', ''],drug_records))
---
+++ Flask_OCR_Server/pymysql_test.py
@@ -0,0 +1,110 @@
+# import pymysql
+
+# # Open database connection
+# db = pymysql.connect(host="localhost", user="username", password="password", database="dbpharmacy")
+
+# # prepare a cursor object using cursor() method
+# cursor = db.cursor()
+
+# # execute SQL query using execute() method.
+# cursor.execute("SELECT drugName,drugDosage FROM tblmedicine")
+
+# # Fetch all rows using fetchall() method.
+# data = cursor.fetchall()
+# print(data)
+# print(type(data))
+
+# # print rows of data
+# for row in data:
+#     print(row)
+
+# # disconnect from server
+# db.close()
+
+####################################################################
+
+# import textdistance
+# import matplotlib.pyplot as plt
+
+test_strings = [
+    'Panadol',         # exact match
+    'Banadyl',         # high similarity
+    'Pandalol',        # high similarity
+    'Kodonal',         # medium similarity
+    'Panaflam',        # low similarity
+    'Ibuprofen',       # very low similarity
+    'Pandalot',        # worst case scenario - only one letter in common
+    'Ampicillin',      # worst case scenario - no letters in common
+    'Pandynol',        # best case scenario - same length and only one transposition
+    'Painadol'         # best case scenario - same length and no transpositions
+]
+
+# panadol = 'Panadol'
+
+# # Compute the Jaro-Winkler distance between each test string and "Panadol"
+# distances = []
+# for test_string in test_strings:
+#     jaro_winkler = textdistance.jaro_winkler(panadol, test_string)
+#     distances.append(jaro_winkler)
+
+# # Plot the distances as a bar graph
+# plt.bar(test_strings, distances)
+# plt.title('Similarity of test strings to "Panadol"')
+# plt.xlabel('Test strings')
+# plt.ylabel('Jaro-Winkler distance')
+# plt.xticks(rotation=90)
+# plt.show()
+
+
+#####################################################################
+
+import matplotlib.pyplot as plt
+import textdistance
+
+# # Define the test strings
+# test_strings = [
+#     "panadol",
+#     "panadole",
+#     "penodol",
+#     "adol",
+#     "paracetamol",
+#     "aspirin",
+#     "ibuprofen",
+#     "tylenol",
+#     "acetaminophen",
+#     "calpol"
+# ]
+
+# Define the reference string
+reference_string = "panadol"
+
+# Compute the similarity scores using various algorithms
+jaro_scores = [textdistance.jaro(reference_string, s) for s in test_strings]
+jaro_winkler_scores = [textdistance.jaro_winkler(reference_string, s) for s in test_strings]
+levenshtein_scores = [textdistance.levenshtein.normalized_similarity(reference_string, s) for s in test_strings]
+
+# Create a bar chart to compare the similarity scores
+fig, ax = plt.subplots()
+x = range(len(test_strings))
+width = 0.25
+
+ax.bar(x, jaro_scores, width, label='Jaro')
+ax.bar([i + width for i in x], jaro_winkler_scores, width, label='Jaro-Winkler')
+ax.bar([i + width*2 for i in x], levenshtein_scores, width, label='Levenshtein')
+
+# Set the chart title and axis labels
+ax.set_title('Similarity of Test Strings to Reference String')
+ax.set_xlabel('Test Strings')
+ax.set_ylabel('Similarity Scores')
+
+# Set the x-axis tick labels
+ax.set_xticks([i + width for i in x])
+ax.set_xticklabels(test_strings)
+
+# Add a legend and show the chart
+plt.legend()
+plt.show()
+
+
+
+
